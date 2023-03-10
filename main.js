07efaa33b971209f05149176775b6c3a7471e038
naazpreetgiani <124805700+naazpreetgiani@users.noreply.github.com>
2023-03-10 09:58:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//Program Title
+document.getElementById("search-btn").addEventListener("click", btnClicked);
+outputE1 = document.getElementById("speed-info");
+
+function btnClicked() {
+  let speed = document.getElementById("speed-in").innerHTML.value;
+  let limit = document.getElementById("limit-in").innerHTML.value;
+}
